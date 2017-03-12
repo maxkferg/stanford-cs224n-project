17f6d168b32b009861f12e88c56444108c7b962f
Max Ferguson <maxkferg@gmail.com>
2017-03-11 20:54:39 -0800

    Fix labelling metric

--- models/paraphrase.py
+++ models/paraphrase.py
@@ -26,29 +26,10 @@ def contrastive_loss(y, d):
     return K.mean(y * K.square(d) + (1 - y) * K.square(K.maximum(margin - d, 0)))
 
 
-def create_pairs(x, digit_indices):
-    """ Positive and negative pair creation.
-        Alternates between positive and negative pairs.
-    """
-    pairs = []
-    labels = []
-    n = min([len(digit_indices[d]) for d in range(10)]) - 1
-    for d in range(10):
-        for i in range(n):
-            z1, z2 = digit_indices[d][i], digit_indices[d][i+1]
-            pairs += [[x[z1], x[z2]]]
-            inc = random.randrange(1, 10)
-            dn = (d + inc) % 10
-            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
-            pairs += [[x[z1], x[z2]]]
-            labels += [1, 0]
-    return np.array(pairs), np.array(labels)
-
-
 def compute_accuracy(predictions, labels):
     """ Compute classification accuracy with a fixed threshold on distances.
     """
-    yhat = predictions > 0.5
+    yhat = predictions < 0.2
     accuracy = (yhat==labels)
     print 'labels',labels
     print 'pred',predictions
