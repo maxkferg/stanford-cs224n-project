88dbdd54b62d34e088823b2e1360ed2e836625e0
Max Ferguson <maxkferg@gmail.com>
2017-03-12 01:32:11 -0800

    Added cos loss

--- models/paraphrase.py
+++ models/paraphrase.py
@@ -5,7 +5,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import *
 from keras.layers import Input,merge
-from keras.optimizers import SGD, RMSprop
+from keras.optimizers import Adam
 from keras import backend as K
 from sklearn.metrics import precision_recall_fscore_support,confusion_matrix
 
@@ -16,7 +16,20 @@ def euclidean_distance(inputs):
     output = K.mean(K.square(inputs[0] - inputs[1]), axis=-1)
     output = K.sqrt(output)
     output = K.expand_dims(output, 1)
+    print K.shape(output)
     return output
+
+
+def l2_normalize(x, axis):
+    norm = K.sqrt(K.sum(K.square(x), axis=axis, keepdims=True))
+    return K.sign(x) * K.maximum(K.abs(x), K.epsilon()) / K.maximum(norm, K.epsilon())
+
+
+def cos_distance(inputs):
+    y_true, y_pred = inputs
+    y_true = K.l2_normalize(y_true, axis=-1)
+    y_pred = K.l2_normalize(y_pred, axis=-1)
+    return K.mean(1 - K.sum((y_true * y_pred), axis=-1))
 
 
 def contrastive_loss(y, d):
@@ -25,6 +38,16 @@ def contrastive_loss(y, d):
     """
     margin = 1
     return K.mean(y * K.square(d) + (1 - y) * K.square(K.maximum(margin - d, 0)))
+
+
+def cos_loss(y,angle):
+    """Custom loss function for cosine distance
+    Values with scores close to zero should match with positive labels (1)
+    Values with scores close to one should match with negative labels (0)
+    """
+    margin = 0.2
+    return K.mean( (1-y) * angle + y*(1-angle))
+
 
 
 def compute_accuracy(yhat, labels):
@@ -49,14 +72,14 @@ class SiameseParaphrase():
         outputRight = autoencoder.midpoints[1]
 
         # merge outputs of the base network and compute euclidean distance
-        lambda_merge = merge([outputLeft, outputRight], mode=euclidean_distance, output_shape=[[None,1]])
+        lambda_merge = merge([outputLeft, outputRight], mode=cos_distance, output_shape=[[None,1]])
 
         # create main network
         model = Model([inputLeft,inputRight],lambda_merge)
 
         # compile
-        rms = RMSprop()
-        model.compile(loss=contrastive_loss, optimizer=rms)
+        optimizer = Adam()
+        model.compile(loss=cos_loss, optimizer=optimizer)
 
         # save
         self.autoencoder = autoencoder
